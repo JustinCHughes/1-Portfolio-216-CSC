b2cca9fba581df1023482a720467c1546cf736bb
JustinCHughes <hcjustin713@gmail.com>
2024-11-21 00:40:32 -0600

    Leaving off at homework problem 29 of chapter 3. Pushing commit and into github to move to laptop

---
+++ Homework/chap3/HW29/HW29.java
@@ -0,0 +1,9 @@
+// Suppose you are given two circularly linked lists, L and M. Describe an
+// algorithm for telling if L and M store the same sequence of elements
+// (but perhaps with different starting points).
+
+package Homework.chap3.HW29;
+
+public class HW29 {
+  
+}

3fdab949cab9bfd278220674c0e6344f0bed8a34
JustinCHughes <hcjustin713@gmail.com>
2024-11-22 20:11:58 -0600

    Finished chap5/HW8

---
+++ Homework/chap5/HW8/HW8.java
@@ -0,0 +1,43 @@
+// Describe a recursive algorithm for converting a string of digits into the
+// integer it represents. For example, '13531' represents the integer 13,531.
+
+// Please reference below code for context
+// 1. I check if input string.length() is == to 0. If so, return 0
+// 2. If the value of the char I'm at in the string is a ',' do not add value
+// 3. To clarify, we are starting from num.length()-1 so we are starting
+//    from the smaller digit place. I resursively call so current value
+//    is added to the next digit to the LEFT * 10. Multiplying by 10 before
+//    adding takes care of having any number place issues.
+
+package Homework.chap5.HW8;
+
+public class HW8 {
+  public static void main(String[] args)
+  {
+    // Standard value
+    System.out.println(toInt("710"));
+
+    // Checking if comma checking is working
+    System.out.println(toInt("150,789,631"));
+  }
+
+  public static int toInt(String num)
+  {
+    if (num.length() == 0)
+    {
+      return 0;
+    }
+    else
+    {
+      char lastChar = num.charAt(num.length() - 1);
+      if (lastChar == ',')
+      {
+        return toInt(num.substring(0, num.length() - 1));
+      }
+      else
+      {
+      return (lastChar - '0') + 10 * toInt(num.substring(0, num.length() - 1));
+      }
+    }
+}
+}
